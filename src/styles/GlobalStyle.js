29e6ab6c721cf5cf74f632114add63d6cd43aab3
Sue Yoon <syy111391@gmail.com>
2022-10-13 22:28:27 +0900

    Add: first commit

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GlobalStyle() {
+  return (
+    <div>
+      <h1>This is Functional Component!</h1>
+    </div>
+  );
+}
+
+export default GlobalStyle;
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function theme() {
+  return (
+    <div>
+      <h1>This is Functional Component!</h1>
+    </div>
+  );
+}
+
+export default theme;
